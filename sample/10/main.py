471cb7e520c60ba95af380a00374f003c8814b62
Solarbeans <140761072+solarsbeans@users.noreply.github.com>
2023-08-29 19:41:44 -0300

    Update main.py

--- sample/10/main.py
+++ sample/10/main.py
@@ -3,7 +3,7 @@ import tkinter.messagebox
 from tkinter.constants import SUNKEN
 
 window=tk.Tk()
-window.title('Claculator')
+window.title('Calc with sync baserow')
 frame=tk.Frame(master=window,bg="skyblue",padx=10)
 frame.pack()
 entry=tk.Entry(master=frame,relief=SUNKEN,borderwidth=3,width=30)
